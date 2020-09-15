026d74107fe2d54da16f48487776b1acc7e98fe3
Xiang Li <lixiangwhuerhy@gmail.com>
2020-09-15 08:55:45 +0800

    update data.

---
+++ Data/build/CMakeFiles/BlendshapeGeneration.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BlendshapeGeneration.dir/src/BlendshapeGenerator.cpp.o"
+  "CMakeFiles/BlendshapeGeneration.dir/src/DeformationTransfer.cpp.o"
+  "CMakeFiles/BlendshapeGeneration.dir/src/MeshTransfer.cpp.o"
+  "CMakeFiles/BlendshapeGeneration.dir/src/TriangleGradient.cpp.o"
+  "libBlendshapeGeneration.a"
+  "libBlendshapeGeneration.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BlendshapeGeneration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Data/build/CMakeFiles/TestCases.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestCases.dir/src/TestCases.cpp.o"
+  "TestCases"
+  "TestCases.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestCases.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
